feb51a3ad089d6fe20da39472afb19acb372fbd2
Gareth Emslie <gareth@soft-cor.es>
2020-08-12 15:24:16 +0200

    simulate a longer job execution time

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+import java.lang.*;
 import java.time.LocalDateTime;
 
 public class App {
@@ -8,6 +8,13 @@ public class App {
     private static final Logger log = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
-        log.info("I AM RUNNING FROM THE JAVA APP. The time is {}", LocalDateTime.now());
+        for (int i = 0; i < 5; i++) {
+            log.info("I AM RUNNING FROM THE JAVA APP. The time is {}", LocalDateTime.now());
+            try {
+                Thread.sleep(60000);
+             } catch (Exception e) {
+                log.error(e);
+             }
+        }
     }
 }
